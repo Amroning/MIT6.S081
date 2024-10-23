29f5e7d0a004bd1e3756c11e73ecf55cc188b9fe
Amroning <645519154@qq.com>
2024-10-23 08:29:38 +0800

    Lab3 work2 completed

--- kernel/proc.c
+++ kernel/proc.c
@@ -25,23 +25,25 @@ extern char trampoline[]; // trampoline.S
 void
 procinit(void)
 {
-  struct proc *p;
-  
-  initlock(&pid_lock, "nextpid");
-  for(p = proc; p < &proc[NPROC]; p++) {
-      initlock(&p->lock, "proc");
+    struct proc *p;
+    
+    initlock(&pid_lock, "nextpid");
+    for(p = proc; p < &proc[NPROC]; p++) {
+        initlock(&p->lock, "proc");
 
-      // Allocate a page for the process's kernel stack.
-      // Map it high in memory, followed by an invalid
-      // guard page.
-      char *pa = kalloc();
-      if(pa == 0)
-        panic("kalloc");
-      uint64 va = KSTACK((int) (p - proc));
-      kvmmap(va, (uint64)pa, PGSIZE, PTE_R | PTE_W);
-      p->kstack = va;
-  }
-  kvminithart();
+        // Allocate a page for the process's kernel stack.
+        // Map it high in memory, followed by an invalid
+        // guard page.
+        //   char *pa = kalloc();
+        //   if(pa == 0)
+        //     panic("kalloc");
+        //   uint64 va = KSTACK((int) (p - proc));
+        //   kvmmap(va, (uint64)pa, PGSIZE, PTE_R | PTE_W);
+        //   p->kstack = va;
+
+        //注释掉了上面的代码（为所有进程预分配内核栈的代码），变为创建进程的时候再创建内核栈，见 allocproc()
+    }
+    kvminithart();
 }
 
 // Must be called with interrupts disabled,
@@ -92,42 +94,53 @@ allocpid() {
 static struct proc*
 allocproc(void)
 {
-  struct proc *p;
+    struct proc *p;
 
-  for(p = proc; p < &proc[NPROC]; p++) {
-    acquire(&p->lock);
-    if(p->state == UNUSED) {
-      goto found;
-    } else {
-      release(&p->lock);
+    for(p = proc; p < &proc[NPROC]; p++) {
+        acquire(&p->lock);
+        if(p->state == UNUSED) {
+        goto found;
+        } else {
+        release(&p->lock);
+        }
     }
-  }
-  return 0;
-
-found:
-  p->pid = allocpid();
-
-  // Allocate a trapframe page.
-  if((p->trapframe = (struct trapframe *)kalloc()) == 0){
-    release(&p->lock);
     return 0;
-  }
 
-  // An empty user page table.
-  p->pagetable = proc_pagetable(p);
-  if(p->pagetable == 0){
-    freeproc(p);
-    release(&p->lock);
-    return 0;
-  }
+    found:
+    p->pid = allocpid();
 
-  // Set up new context to start executing at forkret,
-  // which returns to user space.
-  memset(&p->context, 0, sizeof(p->context));
-  p->context.ra = (uint64)forkret;
-  p->context.sp = p->kstack + PGSIZE;
+    // Allocate a trapframe page.
+    if((p->trapframe = (struct trapframe *)kalloc()) == 0){
+        release(&p->lock);
+        return 0;
+    }
 
-  return p;
+    // An empty user page table.
+    p->pagetable = proc_pagetable(p);
+    if(p->pagetable == 0){
+        freeproc(p);
+        release(&p->lock);
+        return 0;
+    }
+
+    // 为新进程创建独立的内核页表，并将内核所需要的各种映射添加到新页表上
+    p->kernelpgtbl = kvminit_newpgtbl();
+
+    // 分配一个物理页，作为新进程的内核栈使用
+    char* pa = kalloc();
+    if (pa == 0)
+        panic("kallo");
+    uint64 va = KSTACK((int)0);     // 将内核栈映射到固定的逻辑地址上
+    kvmmap(p->kernelpgtbl, va, (uint64)pa, PGSIZE, PTE_R | PTE_W);
+    p->kstack = va;     // 记录内核栈的逻辑地址，其实已经是固定的了，依然这样记录是为了避免需要修改其他部分 xv6 代码
+
+    // Set up new context to start executing at forkret,
+    // which returns to user space.
+    memset(&p->context, 0, sizeof(p->context));
+    p->context.ra = (uint64)forkret;
+    p->context.sp = p->kstack + PGSIZE;
+
+    return p;
 }
 
 // free a proc structure and the data hanging from it,
@@ -136,20 +149,32 @@ found:
 static void
 freeproc(struct proc *p)
 {
-  if(p->trapframe)
-    kfree((void*)p->trapframe);
-  p->trapframe = 0;
-  if(p->pagetable)
-    proc_freepagetable(p->pagetable, p->sz);
-  p->pagetable = 0;
-  p->sz = 0;
-  p->pid = 0;
-  p->parent = 0;
-  p->name[0] = 0;
-  p->chan = 0;
-  p->killed = 0;
-  p->xstate = 0;
-  p->state = UNUSED;
+    if(p->trapframe)
+        kfree((void*)p->trapframe);
+    p->trapframe = 0;
+    if(p->pagetable)
+        proc_freepagetable(p->pagetable, p->sz);
+    p->pagetable = 0;
+    p->sz = 0;
+    p->pid = 0;
+    p->parent = 0;
+    p->name[0] = 0;
+    p->chan = 0;
+    p->killed = 0;
+    p->xstate = 0;
+
+    //释放进程的内核栈
+    void* kstack_pa = (void*)kvmpa(p->kernelpgtbl, p->kstack);
+    kfree(kstack_pa);
+    p->kstack = 0;
+
+    // 此处不能使用 proc_freepagetable，因为其不仅会释放页表本身，还会把页表内所有的叶节点对应的物理页也释放掉。
+    // 这会导致内核运行所需要的关键物理页被释放，从而导致内核崩溃。
+    
+    // 递归释放进程独享的页表，释放页表本身所占用的空间，但不释放页表指向的物理页
+    kvm_free_kernelpgtbl(p->kernelpgtbl);
+    p->kernelpgtbl = 0;
+    p->state = UNUSED;
 }
 
 // Create a user page table for a given process,
@@ -473,7 +498,16 @@ scheduler(void)
         // before jumping back to us.
         p->state = RUNNING;
         c->proc = p;
+
+        // 切换到进程独立的内核页表
+        w_satp(MAKE_SATP(p->kernelpgtbl));
+        sfence_vma();       // 清除快表缓存，刷新TLB缓存，以确保地址转换表的更改生效
+
+         // 调度，执行进程
         swtch(&c->context, &p->context);
+
+        // 切换回全局内核页表
+        kvminithart();
 
         // Process is done running for now.
         // It should have changed its p->state before coming back.
